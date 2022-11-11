ad564963f0b9158d3e2a922668a711b841d50f3e
zelariuz <zelariuz@gmail.com>
2022-11-11 01:37:44 -0300

    [ flask ] last app

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
+import json
+
+def get_api(method, **params):
+	url = "http://ec2-54-173-246-152.compute-1.amazonaws.com:7777/"
+	payload = {
+	    "jsonrpc": "2.0",
+	    "method": method,
+	    "params": params,
+	    "id": 1
+	}
+	headers = {"Content-Type": "application/json"}
+	response = requests.request("GET", url, json=payload, headers=headers)
+	return json.loads(response.text)['result']
+
+print(get_api('alive'))
 
 app = Flask(__name__)
 
@@ -6,6 +22,12 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
 	return render_template("index.html")
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+	data = request.get_json()
+	print(data)
+	return jsonify(get_api('predict', **data))
 
 if __name__ == '__main__':
 	app.run(debug=True)
